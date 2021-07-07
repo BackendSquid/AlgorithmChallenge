b23a448484477fe8734c542e367266ae9f1c3c05
K <PizzaCola.K@gmail.com>
2021-07-07 21:11:31 +0900

    [#1] 짝지어 제거하기 - k

---
+++ 짝지어제거하기/k.py
@@ -0,0 +1,13 @@
+def solution(s):
+    stack = []
+    
+    for c in s:
+        if len(stack) > 0 and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+
+    if len(stack) == 0:
+        return 1
+
+    return 0
