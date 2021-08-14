bb13af2eda70c2e1d4d0e8e96815bbe08ed93817
JiSun Lim <jisunlim818@gmail.com>
2021-08-14 22:29:20 +0900

    [#17] 숫자의 표현 - jane

--- 숫자의표현/jane.py
+++ 숫자의표현/jane.py
@@ -1,9 +1,5 @@
 def solution(n):
     answer = 0
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
     for i in range(1, n // 2 + 1):
         sum = 0
         for j in range(i, n // 2 + 2):
