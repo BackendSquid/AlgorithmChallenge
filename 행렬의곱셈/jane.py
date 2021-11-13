b0f320ca87f8e31fdb0cbd21f482a690960a1038
JiSun Lim <jisunlim818@gmail.com>
2021-11-13 22:01:57 +0900

    행렬의 곱셈 - 풀이 개선

--- 행렬의곱셈/jane.py
+++ 행렬의곱셈/jane.py
@@ -2,12 +2,8 @@ def solution(arr1, arr2):
     answer = [[0] * len(arr2[0]) for _ in range(len(arr1))]
 
     for i in range(len(arr1)):
-        idx = 0
         for j in range(len(arr2[0])):
-            _sum = 0
             for k in range(len(arr1[0])):
-                _sum += arr1[i][k] * arr2[k][idx]
-            answer[i][j] = _sum
-            idx += 1
+                answer[i][j] += arr1[i][k] * arr2[k][j]
             
     return answer
